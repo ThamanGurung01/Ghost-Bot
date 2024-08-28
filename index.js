44ac77dd0590824ba541600c673527ff0a1e2a90
ThamanGurung01 <tgurung519@gmail.com>
2024-08-28 13:16:26 +0545

    ids changed

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const { Client, GatewayIntentBits }=  require('discord.js');
 const schedule = require('node-schedule');
+const fetch=require("node-fetch");
 const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMembers,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
 
-const MyChannelId="1277918596781309967";
-const userId="618625045769748482";
+const MyChannelId="984726930689503255";
+// const userId="618625045769748482";
+const UsersID=require("./user");
 
+let randomId=UsersID[Math.floor(Math.random()*UsersID.length)];
 const SecretName= "Ghost";
 const SecretImageUrl="https://www.clipartmax.com/png/small/3-30793_creepy-discord-icon-logo-remix-by-treetoadart-discord-icon.png";
 
@@ -27,7 +30,7 @@ const UserPing=async()=>{
   try{
 const channel =await client.channels.fetch(MyChannelId);
    if(channel) {
-    const botMessage=await channel.send(`Hello <@${userId}>`);
+    const botMessage=await channel.send(`Hello <@${randomId}>`);
 setTimeout(() => {botMessage.delete();}, 30000);
 setTimeout(PlainMode,120000);
   }
@@ -44,10 +47,43 @@ client.once('ready', async() => {
   schedule.scheduleJob("0 0 * * *",UserPing);
 });
 
+
+//normal functions
+const sadWords=["sad","depressed", "unhappy","angry"];
+
+const encouragements=[
+  "Cheer up!",
+  "Hang in there",
+  "you are a great person/bot",
+]
+
+function getQuote(){
+  return fetch("https://zenquotes.io/api/random").then(res=>{
+    return res.json();
+  }).then(data=>{
+    return data[0]["q"] + " -" + data[0]["a"]
+  })
+}
+
+
 client.on("messageCreate",message=>{
-  console.log(message.content);
   if(message.author.bot) return;
-  message.reply("test");
+  if(message.content.toLowerCase().startsWith("hi")){
+    message.reply({
+      content: "Hi 😊😊"});
+  }else if(message.content.toLowerCase().startsWith("hello")){
+    message.reply({
+      content: "Hello 😊😊"});
+  }
+  if(message.content ==="$inspire"){
+    getQuote().then(quote=> message.reply(quote));
+  }
+
+  if(sadWords.some(word=>message.content.includes(word))){
+    const encouragement=encouragements[Math.floor(Math.random()*encouragements.length)]
+    message.reply(encouragement);
+  }
 })
+
 
 client.login("MTI3Nzk1NzQ4NjkwMDU0NzU5Ng.GMREza.sAWFuZZyB5yezN6ueaul-jPkUdOmdcBpeqBbIM");
