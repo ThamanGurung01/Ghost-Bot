8f8afc762bff0722129900ae8071511fda516a3e
ThamanGurung01 <tgurung519@gmail.com>
2024-08-28 12:13:18 +0545

    Secret mode and Plain mode

--- index.js
+++ index.js
@@ -11,30 +11,37 @@ const SecretImageUrl="https://www.clipartmax.com/png/small/3-30793_creepy-discor
 const oldName="Test-1 Bot";
 const oldImageUrl="https://www.clipartmax.com/png/small/226-2261845_content-creator-influencer-relations-discord-icon-transparent-small.png";
 
+
 const SecretMode=async()=>{
   await client.user.setUsername(SecretName);
   await client.user.setAvatar(SecretImageUrl);
     }
 
+
+ const PlainMode=async()=>{
+      await client.user.setUsername(oldName);
+      await client.user.setAvatar(oldImageUrl);
+    }
+
 const UserPing=async()=>{
   try{
-    const channel =await client.channels.fetch(MyChannelId);
-   if(channel) await channel.send(`Hello <@${userId}>`);
+const channel =await client.channels.fetch(MyChannelId);
+   if(channel) {
+    const botMessage=await channel.send(`Hello <@${userId}>`);
+setTimeout(() => {botMessage.delete();}, 30000);
+setTimeout(PlainMode,120000);
+  }
       }catch(error){
         console.log("error");
       }
 }
 
-const PlainMode=async()=>{
-  await client.user.setUsername(oldName);
-  await client.user.setAvatar(oldImageUrl);
-}
+
 
 client.once('ready', async() => {
   console.log(`Logged in as ${client.user.tag}!`);
-  schedule.scheduleJob("19 22 * * *",SecretMode);
-  schedule.scheduleJob("18 22 * * *",UserPing);
-  schedule.scheduleJob("22 22 * * *",PlainMode);
+  schedule.scheduleJob("57 24 * * *",SecretMode);
+  schedule.scheduleJob("0 0 * * *",UserPing);
 });
 
 client.on("messageCreate",message=>{
